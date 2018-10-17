f484c05fa150f4741eb37e5bf461709aa569f899
unknown <geongeorge98@gmail.com>
2018-10-17 12:59:39 +0530

    Socket io port changed and started working

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+// var server = require('http').Server(app);
+var io = require('socket.io')(3000);
+
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+app.use(cookieParser());
+app.use(session({secret: "lalalala Secret key"}));
 
 //static folder
 const staticfolder = express.static(__dirname + '/static')
@@ -11,5 +19,18 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render("index",{})
 })
+
+app.get('/check', function(req, res){
+    res.send("You are " + req.sessionID );
+ });
+
+ io.on('connection', function (socket) {
+    socket.emit('newmsg', { msg: 'Hey' });
+    socket.on('newmsg', function (data) {
+        console.log(data.msg);
+        socket.broadcast.emit('newmsg', { msg: data.msg, uid: data.uid});
+      });
+  });
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
